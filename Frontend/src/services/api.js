b9c2e2cb35a97ff592cfa435d9542eb471e707b6
Prathamesh Sonar <pratham25092004@gmail.com>
2025-08-12 12:29:17 +0530

    Update api.js

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api', // The vite proxy will handle this
+  baseURL: `${import.meta.env.VITE_API_URL}/api`
+  // baseURL: '/api', // The vite proxy will handle this
 });
 
 // We can add interceptors here later to handle token refresh if needed
