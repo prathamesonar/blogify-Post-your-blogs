b41578e2d30aa46eb5a2c5c80a43394e49761a39
Prathamesh Sonar <pratham25092004@gmail.com>
2025-08-12 10:26:34 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://blogify-post-your-blogs.onrender.com'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
