9d129c841632aa58cef28a82e5823a0e89a9588b
Prathamesh Sonar <pratham25092004@gmail.com>
2025-08-12 22:35:44 +0530

    Update userModel.js

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -3,8 +3,8 @@ const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  username: { type: String, required: true, unique: true, trim: true },
-  email: { type: String, required: true, unique: true, trim: true },
+  username: { type: String, required: true, unique: true, trim: true, index: true },
+  email: { type: String, required: true, unique: true, trim: true, index: true },
   password: { type: String, required: true, minlength: 6 },
   profilePic: { type: String, default: '' },
   followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
